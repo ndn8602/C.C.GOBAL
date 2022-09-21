4b40fedc708010ad74c6821148c3887835727be6
Nham <nguyenducnhamndn@gmail.com>
2022-09-21 21:12:21 +0700

    update

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,3 @@
-let heighTop = window.scrollY;
-console.log(heighTop);
 var Scrollbar = window.Scrollbar;
 options = {
   damping: 0.1,
@@ -11,15 +9,6 @@ let scrollbar = Scrollbar.init(
 
 scrollbar.setPosition(0, 0);
 scrollbar.track.xAxis.element.remove();
-
-function listener(status) {
-  let scroll = scrollbar.offset.y;
-  heighTop = scroll;
-}
-scrollbar.addListener(listener);
-setInterval(() => {
-  console.log(heighTop);
-}, 3000);
 //Handle offset Scroll
 AOS.init({
   delay: 500,
@@ -30,9 +19,4 @@ AOS.init({
       el.classList.add("aos-animate");
     }
   });
-});
-
-console.log(document.documentElement.scrollTop || document.body.scrollTop);
-$(window).scroll(() => {
-  console.log(document.documentElement.scrollTop || document.body.scrollTop);
 });
