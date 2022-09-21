20bf49c56936705da2652851c9eaa23804a4ffe8
Nham <nguyenducnhamndn@gmail.com>
2022-09-22 00:31:49 +0700

    CCGOBAL

--- JS/app.js
+++ JS/app.js
@@ -6,13 +6,29 @@ let scrollbar = Scrollbar.init(
   document.querySelector("#my-scrollbar"),
   options
 );
+const header = $(".header").offset().top;
+const content = $(".content").offset().top;
+const contact = $(".contact").offset().top;
 
 scrollbar.setPosition(0, 0);
 scrollbar.track.xAxis.element.remove();
+function headerTop() {
+  scrollbar.scrollTo(0, header-100, 1000);
+  console.log("header");
+}
+function contentTop() {
+  scrollbar.scrollTo(0, content-100, 1000);
+  console.log("content");
+}
+function contactTop() {
+  scrollbar.scrollTo(0, contact-100, 1000);
+  console.log("contact");
+}
 //scroll animattion
 function listener(status) {
   let scroll = scrollbar.offset.y;
   console.log(scroll);
+
   if (scroll >= 300) {
     $(".navigation").removeClass("d-none");
   } else {
@@ -20,6 +36,19 @@ function listener(status) {
   }
 }
 scrollbar.addListener(listener);
+$(".navbar-toggler").click(() => {
+  if ($("#navbarNav").hasClass("show")) {
+    $(".navbar").css("background-color", "transparent ");
+    console.log("ok");
+    console.log($("#navbarNav").hasClass("show"));
+  } else {
+    $(".navbar").css("background-color", "#44463d");
+    $(".navbar").css("z-index", "9999");
+    console.log("not ok");
+    console.log($("#navbarNav").hasClass("show"));
+  }
+});
+
 //Handle offset Scroll
 AOS.init({
   delay: 500,
